c26f9c517c370b08f470781cb1c46b126226d390
alruiz12 <alvaro-ruiz-95@hotmail.com>
2017-09-27 12:13:59 +0200

    kmeans single threaded v0 before main

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,6 +3,11 @@
  */
 public class hello {
     public static void main(String[] args) {
+
         System.out.println("hello");
+        class aux{
+
+        }
+
     }
 }
---
+++ src/main/java/kmeans/Main.java
@@ -0,0 +1,7 @@
+package kmeans;
+
+/**
+ * Created by alvaro on 27/09/17.
+ */
+public class Main {
+}

b7f33cfcaacf96b6ff073019440cc3fb39d06efa
alruiz12 <alvaro-ruiz-95@hotmail.com>
2017-10-16 00:45:53 +0200

    scripts updated and adapted to amazon EC2

--- localParallelize.sh
+++ localParallelize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-java -cp /home/alvaro/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/alvaro/Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main >A &>A &
-java -cp /home/alvaro/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/alvaro/Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main3 >B &>B &
-java -cp /home/alvaro/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/alvaro/Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main3 >C &>C &
-java -cp /home/alvaro/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/alvaro/Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main3 >D &>D &
-java -cp /home/alvaro/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/alvaro/Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main3 >E &>E &
-java -cp /home/alvaro/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/alvaro/Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main3 >F &>F &
+java -cp /home/alvaro/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/alvaro/Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main $1 >A &>A &
+num="$(($1-1 ))"
+for i in $(seq 1 $num); do
+    java -cp /home/alvaro/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/alvaro/Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main3 $1 >$i &>$i &
+    echo $i
+done
 
--- parallelize.sh
+++ parallelize.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:. main </dev/null &>/dev/null &
-java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:. main3 </dev/null &>/dev/null &
-java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:. main3 </dev/null &>/dev/null &
-java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:. main3 </dev/null &>/dev/null &
-java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:. main3 </dev/null &>/dev/null &
-java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:. main3 </dev/null &>/dev/null &
+
+java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/ubuntu//Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main $1 >A &>A &
+num="$(($1-1 ))"
+for i in $(seq 1 $num); do
+    java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/ubuntu//Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main3 $1 >$i &>$i &
+    echo $i
+done
+
 
 

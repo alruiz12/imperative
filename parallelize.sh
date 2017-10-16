80e4d9df89674c40eb8d65958d401b548fc218f8
Alvaro Ruiz <ubuntu@ip-172-31-17-45.eu-west-2.compute.internal>
2017-10-16 00:54:02 +0000

    script modified

--- parallelize.sh
+++ parallelize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/ubuntu//Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main $1 >A &>A &
+java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/ubuntu/java/imperative/hazelcast-client-3.8.6.jar:. main $1 >A &>A &
 num="$(($1-1 ))"
 for i in $(seq 1 $num); do
-    java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/ubuntu//Downloads/hazelcast-3.8.6/lib/hazelcast-client-3.8.6.jar:. main3 $1 >$i &>$i &
+    java -cp /home/ubuntu/.m2/repository/com/hazelcast/hazelcast/3.8.6/hazelcast-3.8.6.jar:/home/ubuntu/java/imperative/hazelcast-client-3.8.6.jar:. main3 $1 >$i &>$i &
     echo $i
 done
 

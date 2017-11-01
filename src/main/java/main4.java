1961957bf8e13321f3d48e564c31f556063ad6b2
alruiz12 <alvaro-ruiz-95@hotmail.com>
2017-11-01 20:34:57 +0100

    about to time with more data

--- src/main/java/main4.java
+++ src/main/java/main4.java
@@ -11,9 +11,9 @@ public class main4 {
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
-            final int NUM_CLUSTERS = 10;
+            final int NUM_CLUSTERS = 150;
             //Number of Points
-            final int NUM_POINTS = 100;
+            final int NUM_POINTS = 1500;
             //Min and Max X and Y
             final int MIN_COORDINATE = 0;
             final int MAX_COORDINATE = 10000;
